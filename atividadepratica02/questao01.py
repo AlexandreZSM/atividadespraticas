1b2c94786273e7e4a1dc1aead180209072c31788
AlexandreZSM <xande20x@gmail.com>
2025-08-21 09:43:48 -0300

    Atividade prática 2 - Questão 1

--- atividadepratica02/questao01.py
+++ atividadepratica02/questao01.py
@@ -1,10 +1,10 @@
-valor_em_rais = 100.00
+valor_em_rais = float(input("Digite o valor em reais: "))
 taxa_dolar = 5.20
 taxa_euro = 6.15
 
 valor_em_dolar = valor_em_rais / taxa_dolar
 valor_em_euro = valor_em_rais / taxa_euro
 
-print("Valor em reais: R$" valor_em_rais)
+print("Valor em reais: R$", valor_em_rais)
 print("Valor em dólar: U$", round(valor_em_dolar, 2))
 print("Valor em euro: €", round(valor_em_euro, 2))
